7b13652e7d969127f7057866238c9bf816fb40b6
Sai Vanamala <saivanamala714@gmail.com>
2019-01-20 20:19:37 -0500

    Local DB Project

---
+++ localdb/src/main/java/com/vsstore/localdb/LocaldbApplication.java
@@ -0,0 +1,14 @@
+package com.vsstore.localdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaldbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocaldbApplication.class, args);
+    }
+
+}
+
